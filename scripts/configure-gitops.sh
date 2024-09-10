8139433658412c33fea4a2ec20e0f17902114d87
Jordan Dubrick <jdubrick@redhat.com>
2024-09-10 13:44:22 -0400

    more review comments
    Signed-off-by: Jordan Dubrick <jdubrick@redhat.com>

--- scripts/configure-gitops.sh
+++ scripts/configure-gitops.sh
@@ -6,12 +6,14 @@ RHDH_DEPLOYMENT="backstage-ai-rh-developer-hub" # deployment created by rhdh ope
 DEFAULT_PLUGIN_CONFIGMAP="backstage-dynamic-plugins-ai-rh-developer-hub" # configmap created by rhdh operator for plugins by default
 DEFAULT_SECRET_NAME="rhdh-argocd-secret"
 
+BASE_DIR="$(realpath $(dirname ${BASH_SOURCE[0]}))/.."
+
 # ArgoCD Instance Created By Installer
 if [[ $ARGOCD_INSTANCE_PROVIDED == "false" ]]; then
     # Add ConfigMap To Configure ArgoCD
-    kubectl apply -n $NAMESPACE -f ./resources/argocd-config.yaml
-    # Add COnfigMap For ArgoCD Plugins
-    kubectl apply -n $NAMESPACE -f ./resources/argocd-plugins.yaml
+    kubectl apply -n $NAMESPACE -f $BASE_DIR/resources/argocd-config.yaml
+    # Add ConfigMap For ArgoCD Plugins
+    kubectl apply -n $NAMESPACE -f $BASE_DIR/resources/argocd-plugins.yaml
 
     # Grab configmap and parse out the defined yaml file inside of its data to a temp file
     kubectl get configmap $DEFAULT_PLUGIN_CONFIGMAP -n $NAMESPACE -o yaml | yq '.data["dynamic-plugins.yaml"]' > temp-dynamic-plugins.yaml
@@ -38,43 +40,100 @@ if [[ $ARGOCD_INSTANCE_PROVIDED == "false" ]]; then
     kubectl apply -f -
 fi
 
+EXISTING_NAMESPACE=${EXISTING_NAMESPACE:-''}
+EXISTING_DEPLOYMENT=${EXISTING_DEPLOYMENT:-''}
+RHDH_PLUGINS=${RHDH_PLUGINS:-''}
+ARGO_USERNAME=${ARGO_USERNAME:-''}
+ARGO_PASSWORD=${ARGO_PASSWORD:-''}
+ARGO_HOSTNAME=${ARGO_HOSTNAME:-''}
+ARGO_TOKEN=${ARGO_TOKEN:-''}
+ARGO_INSTANCE_NAME=${ARGO_INSTANCE_NAME:-''}
+
 # ArgoCD Instance Brought By User
 # If a user is using this step it is assumed they did not use our installer so we need ALL the information required with no assumptions
 if [[ $ARGOCD_INSTANCE_PROVIDED == "true" ]]; then
     # Gather ArgoCD instance information
     echo "You have chosen to provide your own ArgoCD instance"
-    read -p "Enter the namespace of your RHDH and ArgoCD instance: " NAMESPACE
-    read -p "Enter the deployment name of your RHDH instance: " RHDH_DEPLOYMENT
-    read -p "Enter the name of the ConfigMap for your RHDH plugins: " RHDH_PLUGINS
-    read -p "Enter your ArgoCD username: " ARGO_USERNAME
-    read -p "Enter password for $ARGO_USERNAME: " ARGO_PASSWORD
-    read -p "Enter your ArgoCD hostname: " ARGO_HOSTNAME
-    read -p "Enter your ArgoCD token: " ARGO_TOKEN
 
-    read -p "Enter a name for your ArgoCD instance: " ARGOCD_INSTANCE_NAME
+    until [ ! -z "${EXISTING_NAMESPACE}" ]; do
+        read -p "Enter the namespace of your RHDH and ArgoCD instance: " EXISTING_NAMESPACE
+        if [ -z "${EXISTING_NAMESPACE}" ]; then
+            echo "No namespace entered, try again."
+        fi
+    done
+
+    until [ ! -z "${EXISTING_DEPLOYMENT}" ]; do
+        read -p "Enter the deployment name of your RHDH instance: " EXISTING_DEPLOYMENT
+        if [ -z "${EXISTING_DEPLOYMENT}" ]; then
+            echo "No deployment entered, try again."
+        fi
+    done
+
+    until [ ! -z "${RHDH_PLUGINS}" ]; do
+        read -p "Enter the name of the ConfigMap for your RHDH plugins: " RHDH_PLUGINS
+        if [ -z "${RHDH_PLUGINS}" ]; then
+            echo "No ConfigMap entered, try again."
+        fi
+    done
+
+    until [ ! -z "${ARGO_USERNAME}" ]; do
+        read -p "Enter your ArgoCD username: " ARGO_USERNAME
+        if [ -z "${ARGO_USERNAME}" ]; then
+            echo "No username entered, try again."
+        fi
+    done
+
+    until [ ! -z "${ARGO_PASSWORD}" ]; do
+        read -p "Enter password for $ARGO_USERNAME: " ARGO_PASSWORD
+        if [ -z "${ARGO_PASSWORD}" ]; then
+            echo "No password entered, try again."
+        fi
+    done
+
+    until [ ! -z "${ARGO_HOSTNAME}" ]; do
+        read -p "Enter your ArgoCD hostname: " ARGO_HOSTNAME
+        if [ -z "${ARGO_HOSTNAME}" ]; then
+            echo "No hostname entered, try again."
+        fi
+    done
+    
+    until [ ! -z "${ARGO_TOKEN}" ]; do
+        read -p "Enter your ArgoCD token: " ARGO_TOKEN
+        if [ -z "${ARGO_TOKEN}" ]; then
+            echo "No token entered, try again."
+        fi
+    done
+    
+    until [ ! -z "${ARGO_INSTANCE_NAME}" ]; do
+        read -p "Enter a name for your ArgoCD instance: " ARGO_INSTANCE_NAME
+        if [ -z "${ARGO_INSTANCE_NAME}" ]; then
+            echo "No ArgoCD instance entered, try again."
+        fi
+    done
+    
     echo "Creating ArgoCD secret file"
     # Create the secret in the namespace so we can connect it to rhdh TODO: need the setup namespace input
-    kubectl create secret generic "$ARGOCD_INSTANCE_NAME-secret" \
+    kubectl create secret generic "$ARGO_INSTANCE_NAME-secret" \
             --from-literal="ARGOCD_API_TOKEN=$ARGO_TOKEN" \
             --from-literal="ARGOCD_HOSTNAME=$ARGO_HOSTNAME" \
             --from-literal="ARGOCD_PASSWORD=$ARGO_PASSWORD" \
             --from-literal="ARGOCD_USER=$ARGO_USERNAME" \
-            -n "$NAMESPACE" \
+            -n "$EXISTING_NAMESPACE" \
             > /dev/null
     
     echo "Applying ArgoCD ConfigMaps"
     # Create the plugin and config setup configmaps in the namespace
-    kubectl apply -n $NAMESPACE -f ./resources/argocd-config.yaml
-    kubectl apply -n $NAMESPACE -f ./resources/argocd-plugins.yaml
+    kubectl apply -n $EXISTING_NAMESPACE -f $BASE_DIR/resources/argocd-config.yaml
+    kubectl apply -n $EXISTING_NAMESPACE -f $BASE_DIR/resources/argocd-plugins.yaml
     
     # Grab configmap and parse out the defined yaml file inside of its data to a temp file
-    kubectl get configmap $RHDH_PLUGINS -n $NAMESPACE -o yaml | yq '.data["dynamic-plugins.yaml"]' > temp-dynamic-plugins.yaml
+    kubectl get configmap $RHDH_PLUGINS -n $EXISTING_NAMESPACE -o yaml | yq '.data["dynamic-plugins.yaml"]' > temp-dynamic-plugins.yaml
 
     # Edit the temp file to include the argocd plugins
     yq -i '.includes += ["argocd-plugins.yaml"] | .includes |= unique' temp-dynamic-plugins.yaml
 
     # Patch the configmap that is deployed to update the defined yaml inside of it
-    kubectl patch configmap $RHDH_PLUGINS -n $NAMESPACE \
+    kubectl patch configmap $RHDH_PLUGINS -n $EXISTING_NAMESPACE \
     --type='merge' \
     -p="{\"data\":{\"dynamic-plugins.yaml\":\"$(echo "$(cat temp-dynamic-plugins.yaml)" | sed 's/"/\\"/g' | sed 's/$/\\n/g' | tr -d '\n')\"}}"
     
@@ -82,18 +141,18 @@ if [[ $ARGOCD_INSTANCE_PROVIDED == "true" ]]; then
     rm -rf temp-dynamic-plugins.yaml
 
     # Check if envFrom is present since Helm installs don't have this field by default
-    has_field=$(kubectl get deploy $RHDH_DEPLOYMENT -n $NAMESPACE -o yaml | \
+    has_field=$(kubectl get deploy $EXISTING_DEPLOYMENT -n $NAMESPACE -o yaml | \
     yq '.spec.template.spec.containers[0] | has("envFrom")')
 
     if [[ $has_field == "false" ]]; then
-        kubectl get deploy $RHDH_DEPLOYMENT -n $NAMESPACE -o yaml | \
+        kubectl get deploy $EXISTING_DEPLOYMENT -n $EXISTING_NAMESPACE -o yaml | \
         yq '.spec.template.spec.containers[0].envFrom = []' |
         kubectl apply -f -
     fi
 
     # Add ArgoCD instance information and plugin to backstage deployment data
-    kubectl get deploy $RHDH_DEPLOYMENT -n $NAMESPACE -o yaml | \
-    secretenv="$ARGOCD_INSTANCE_NAME-secret" yq '.spec.template.spec.volumes += [{"name": "argocd-config", "configMap": {"name": "argocd-config", "defaultMode": 420, "optional": false}}, 
+    kubectl get deploy $EXISTING_DEPLOYMENT -n $EXISTING_NAMESPACE -o yaml | \
+    secretenv="$ARGO_INSTANCE_NAME-secret" yq '.spec.template.spec.volumes += [{"name": "argocd-config", "configMap": {"name": "argocd-config", "defaultMode": 420, "optional": false}}, 
     {"name": "argocd-plugins", "configMap": {"name": "argocd-plugins", "defaultMode": 420, "optional": false}}] |
     .spec.template.spec.containers[0].volumeMounts += [{"name": "argocd-config", "readOnly": true, "mountPath": "/opt/app-root/src/argocd-config.yaml", "subPath": "argocd-config.yaml"}] |
     .spec.template.spec.containers[0].args += ["--config", "/opt/app-root/src/argocd-config.yaml"] |
