b81110286d91d5ab590b0b171955de58cf69642c
Michael Valdron <mvaldron@redhat.com>
2024-09-11 18:29:42 -0400

    tekton plugins
    Signed-off-by: Michael Valdron <mvaldron@redhat.com>

--- scripts/configure-pipelines.sh
+++ scripts/configure-pipelines.sh
@@ -1,18 +1,34 @@
 #!/bin/bash
 
+# Constants
+DEFAULT_RHDH_DEPLOYMENT="backstage-ai-rh-developer-hub" # deployment created by rhdh operator by default
+PLUGIN_CONFIGMAP="backstage-dynamic-plugins-ai-rh-developer-hub" # configmap created by rhdh operator for plugins by default
+CRD="tektonconfigs"
+
 # Variables
-BASE_DIR="$(realpath $(dirname ${BASH_SOURCE[0]}))"
+BASE_DIR="$(realpath $(dirname ${BASH_SOURCE[0]}))/.."
+RHDH_DEPLOYMENT="${DEFAULT_RHDH_DEPLOYMENT}"
 NAMESPACE=${NAMESPACE:-"ai-rhdh"}
 PIPELINES_NAMESPACE=${PIPELINES_NAMESPACE:-"openshift-pipelines"}
 PIPELINES_SECRET_NAME=${PIPELINES_SECRET_NAME:-'rhdh-pipelines-secret'}
+EXISTING_NAMESPACE=${EXISTING_NAMESPACE:-''}
+EXISTING_DEPLOYMENT=${EXISTING_DEPLOYMENT:-''}
+RHDH_INSTANCE_PROVIDED=${RHDH_INSTANCE_PROVIDED:-false}
 
 # Secret variables
 GITHUB__APP__ID=${GITHUB__APP__ID:-''}
 GITHUB__APP__WEBHOOK__SECRET=${GITHUB__APP__WEBHOOK__SECRET:-''}
 GITHUB__APP__PRIVATE_KEY=${GITHUB__APP__PRIVATE_KEY:-''}
 
-# Constants
-CRD="tektonconfigs"
+# Use existing variables if RHDH instance is provided
+if [[ $RHDH_INSTANCE_PROVIDED != "true" ]] && [[ $RHDH_INSTANCE_PROVIDED != "false" ]]; then
+    echo -n "RHDH_INSTANCE_PROVIDED needs to be set to either 'true' or 'false'"
+    echo "FAIL"
+    exit 1
+elif [[ $RHDH_INSTANCE_PROVIDED == "true" ]]; then
+    NAMESPACE="${EXISTING_NAMESPACE}"
+    RHDH_DEPLOYMENT="${EXISTING_DEPLOYMENT}"
+fi
 
 # Reading secrets
 # Reads secrets either from environment variables or user input
@@ -83,7 +99,7 @@ until kubectl get tektonconfig config >/dev/null 2>&1; do
     echo -n "."
     sleep 3
 done
-TEKTON_CONFIG=$(jq ".spec.chain.\"transparency.url\" = \"http://rekor-server.${NAMESPACE}.svc\"" $BASE_DIR/../resources/tekton-config.json -Mc)
+TEKTON_CONFIG=$(jq ".spec.chain.\"transparency.url\" = \"http://rekor-server.${NAMESPACE}.svc\"" $BASE_DIR/resources/tekton-config.json -Mc)
 kubectl patch tektonconfig config --type 'merge' --patch "${TEKTON_CONFIG}" >/dev/null
 if [ $? -ne 0 ]; then
     echo "FAIL"
@@ -118,5 +134,60 @@ if [ "$(kubectl get secret -n "${PIPELINES_NAMESPACE}" "pipelines-as-code-secret
         echo "FAIL"
         exit 1
     fi
+fi
+echo "OK"
+
+# Applying Tekton plugins
+# Creates ConfigMap for list of Tekton plugins
+echo -n "* Applying Tekton plugins: "
+kubectl -n ${NAMESPACE} apply -f $BASE_DIR/resources/tekton-plugins.yaml
+if [ $? -ne 0 ]; then
+    echo "FAIL"
+    exit 1
+fi
+
+# Grab configmap and parse out the defined yaml file inside of its data to a temp file
+kubectl get configmap $PLUGIN_CONFIGMAP -n $NAMESPACE -o yaml | yq '.data["dynamic-plugins.yaml"]' > temp-dynamic-plugins.yaml
+if [ $? -ne 0 ]; then
+    echo "FAIL"
+    exit 1
+fi
+
+# Edit the temp file to include the tekton plugins
+yq -i '.includes += ["tekton-plugins.yaml"] | .includes |= unique' temp-dynamic-plugins.yaml
+if [ $? -ne 0 ]; then
+    echo "FAIL"
+    exit 1
+fi
+
+# Patch the configmap that is deployed to update the defined yaml inside of it
+kubectl patch configmap $PLUGIN_CONFIGMAP -n $NAMESPACE \
+--type='merge' \
+-p="{\"data\":{\"dynamic-plugins.yaml\":\"$(echo "$(cat temp-dynamic-plugins.yaml)" | sed 's/"/\\"/g' | sed 's/$/\\n/g' | tr -d '\n')\"}}"
+if [ $? -ne 0 ]; then
+    echo "FAIL"
+    exit 1
+fi
+
+# Cleanup temp files
+rm temp-dynamic-plugins.yaml
+
+echo "OK"
+
+# Add Tekton information and plugin to backstage deployment data
+echo -n "* Adding Tekton information and plugin to backstage deployment data: "
+K8S_SA_SECRET_NAME=$(kubectl get secrets -n "$NAMESPACE" -o name | grep rhdh-kubernetes-plugin-token- | cut -d/ -f2 | head -1)
+if [ $? -ne 0 ]; then
+    echo "FAIL"
+    exit 1
+fi
+kubectl get deploy $RHDH_DEPLOYMENT -n $NAMESPACE -o yaml | \
+    yq ".spec.template.spec.volumes += [{\"name\": \"tekton-plugins\", \"configMap\": {\"name\": \"tekton-plugins\", \"defaultMode\": 420, \"optional\": false}}] |
+    .spec.template.spec.initContainers[0].env += [{\"name\": \"K8S_SA_TOKEN\", \"valueFrom\": {\"secretKeyRef\": {\"name\": \"${K8S_SA_SECRET_NAME}\", \"key\": \"token\"}}}] |
+    .spec.template.spec.initContainers[0].volumeMounts += [{\"name\": \"tekton-plugins\", \"readOnly\": true, \"mountPath\": \"/opt/app-root/src/tekton-plugins.yaml\", \"subPath\": \"tekton-plugins.yaml\"}]" | \
+    kubectl apply -f -
+if [ $? -ne 0 ]; then
+    echo "FAIL"
+    exit 1
 fi
 echo "OK"
